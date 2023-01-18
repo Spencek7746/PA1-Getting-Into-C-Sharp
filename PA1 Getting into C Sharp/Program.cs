d514503cbcce5a2a2ebf2ccb78bb5e88986dd7f7
Spencer Karpati <spebkarp@ut.utm.edu>
2023-01-17 18:59:36 -0600

    Added class implementations.

--- PA1 Getting into C Sharp/Program.cs
+++ PA1 Getting into C Sharp/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿//Name: Spencer Karpati
+//Instructor: Dr. Kathleen Ericson
+//Class: CSCI 352
+//Description: This program helps with learning C# and the concept of abstract and concrete classes.
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +13,74 @@ namespace PA1_Getting_into_C_Sharp
 {
     internal class Program
     {
-        static void Main(string[] args)
+
+        abstract public class Animal
+        {
+            private String Name;
+            private int Age;
+            private String Noise;
+            private int Weight;
+
+            public abstract void printInfo();
+            public abstract void makeNoise();
+            public abstract int ageUp();
+        }
+
+        public class Cat : Animal
+        {
+            public override void printInfo()
+            {
+                throw new NotImplementedException();
+            }
+
+            public override void makeNoise()
+            {
+                throw new NotImplementedException();
+            }
+
+            public override int ageUp()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+            public class Cassowary : Animal
+            {
+                public override void printInfo()
+                {
+                    throw new NotImplementedException();
+                }
+
+                public override void makeNoise()
+                {
+                    throw new NotImplementedException();
+                }
+
+                public override int ageUp()
+                {
+                    throw new NotImplementedException();
+                }
+            }
+
+                public class Austin_Powers : Animal
+                {
+                    public override void printInfo()
+                    {
+                        throw new NotImplementedException();
+                    }
+
+                    public override void makeNoise()
+                    {
+                        throw new NotImplementedException();
+                    }
+
+                    public override int ageUp()
+                    {
+                        throw new NotImplementedException();
+                    }
+                }
+
+                    static void Main(string[] args)
         {
         }
     }
